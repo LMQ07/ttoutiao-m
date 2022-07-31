d7d9b7ac4a1063078e3e2ddd4d031b58931b8c3b
summer-MQ <mq1009416078@163.com>
2022-07-31 10:15:22 +0800

    一级路由已完成

--- src/router/index.js
+++ src/router/index.js
@@ -7,11 +7,32 @@ const routes = [
   {
     path: '/login',
     // component: Login
-    component: () => import('@/views/Login')
+    component: () => import('@/views/login')
   },
   {
-    path: '/profile',
-    component: () => import('@/views/profile')
+    // 二级路由当中如果没有/ 那么就是会和一级路由进行拼接 假设一级路由 /my 二级路由 home 那么就是/my/home
+    // 二级路由当中如果有/ 那么就是不会和一级路由进行拼接
+    path: '/',
+    component: () => import('@/views/layout'),
+    redirect: '/ ',
+    children: [
+      {
+        path: '/ ',
+        component: () => import('@/views/home')
+      },
+      {
+        path: '/video',
+        component: () => import('@/views/video')
+      },
+      {
+        path: '/qa',
+        component: () => import('@/views/qa')
+      },
+      {
+        path: '/my',
+        component: () => import('@/views/my')
+      }
+    ]
   }
 ]
 const router = new VueRouter({
---
+++ src/views/layout/index.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <!-- 二级路由 -->
+    <router-view></router-view>
+    <!-- 底部导航 -->
+    <!-- route 开启路由模式 -->
+    <!-- 拿到path 和每一个 tabbaritem的to属性对比 一致就高亮-->
+    <van-tabbar route>
+      <van-tabbar-item
+        v-for="(item, index) in tabbarData"
+        :key="index"
+        :to="item.to"
+      >
+        <template>
+          <span :class="`toutiao ${item.icon}`"></span>
+          <span>{{ item.text }}</span>
+        </template>
+      </van-tabbar-item>
+      <!-- <van-tabbar-item to="/video">
+        <template>
+          <span class="toutiao toutiao-shipin"></span>
+          <span>视频</span>
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item to="/qa">
+        <template>
+          <span class="toutiao toutiao-wenda"></span>
+          <span>问答</span>
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item to="/my">
+        <template>
+          <span class="toutiao toutiao-wode"></span>
+          <span>我的</span>
+        </template>
+      </van-tabbar-item> -->
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // 底部导航的数据
+      tabbarData: [
+        {
+          to: '/ ',
+          icon: 'toutiao-shouye1',
+          text: '首页'
+        },
+        {
+          to: '/video',
+          icon: 'toutiao-shipin',
+          text: '视频'
+        },
+        {
+          to: '/qa',
+          icon: 'toutiao-wenda',
+          text: '问答'
+        },
+        {
+          to: '/my',
+          icon: 'toutiao-wode',
+          text: '我的'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+:deep(.van-tabbar-item__text) {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  height: 100%;
+  font-size: 30px;
+  .toutiao {
+    font-size: 40px;
+  }
+}
+</style>
